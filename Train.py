853fc2e9fa2da5645c341ed4506c04d3f283104d
Qifan Zhang <zqifan73@gmail.com>
2026-04-14 08:51:48 +0800

    Refactor Train.py for improved structure and clarity
    Refactor training script by consolidating functions, improving weight loading, and enhancing loss calculations.

--- Train.py
+++ Train.py
@@ -1,243 +1,184 @@
 import torch
-from torch.autograd import Variable
 import os
 import argparse
+import logging
+import numpy as np
 from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
+import torch.nn.functional as F
+
 from backbone.PLCamo import MyNet
 from utils.dataloader import get_loader, test_dataset
 from utils.utils import clip_gradient, adjust_lr, AvgMeter
-import torch.nn.functional as F
-import numpy as np
-import logging
-from torch.utils.tensorboard import SummaryWriter
-import matplotlib.pyplot as plt
-####
-####CUDA_VISIBLE_DEVICES=0 python3 Train.py
-####
-def load_matched_state_dict(model, state_dict, print_stats=True):
-    """
-    Only loads weights that matched in key and shape. Ignore other weights.
-    """
-    num_matched, num_total = 0, 0
-    curr_state_dict = model.state_dict()
-    for key in curr_state_dict.keys():
-        num_total += 1
-        if key in state_dict and curr_state_dict[key].shape == state_dict[key].shape:
-            curr_state_dict[key] = state_dict[key]
-            num_matched += 1
-    model.load_state_dict(curr_state_dict)
-    if print_stats:
-        print(f'Loaded state_dict: {num_matched}/{num_total} matched')
 
+def load_partial_weights(model, state_dict, verbose=True):
+    own_state = model.state_dict()
+    matched = total = 0
 
-def structure_loss(pred, mask):
-    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
-    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
-    wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
+    for key in own_state:
+        total += 1
+        if key in state_dict and own_state[key].shape == state_dict[key].shape:
+            own_state[key].copy_(state_dict[key])
+            matched += 1
+
+    model.load_state_dict(own_state)
+    if verbose:
+        print(f"Loaded weights: {matched}/{total} layers matched")
+
+def weighted_loss(pred, mask):
+    weight = 1 + 5 * torch.abs(F.avg_pool2d(mask, 31, 1, 15) - mask)
+    bce = F.binary_cross_entropy_with_logits(pred, mask, reduction='none')
+    bce = (weight * bce).sum(dim=(2, 3)) / weight.sum(dim=(2, 3))
 
     pred = torch.sigmoid(pred)
-    inter = ((pred * mask) * weit).sum(dim=(2, 3))
-    union = ((pred + mask) * weit).sum(dim=(2, 3))
-    wiou = 1 - (inter + 1) / (union - inter + 1)
+    inter = (pred * mask * weight).sum(dim=(2, 3))
+    union = (pred + mask * weight).sum(dim=(2, 3))
+    iou = 1 - (inter + 1) / (union - inter + 1)
 
-    return (wbce + wiou).mean()
+    return (bce + iou).mean()
 
+def compute_layered_loss(outputs, target, gamma=0.2):
+    losses = [weighted_loss(out, target) for out in outputs]
+    total = 0
+    for idx, loss in enumerate(losses):
+        total += gamma * idx * loss
+    return total
 
-def val(model, epoch, save_path, writer):
-    """
-    validation function
-    """
-    global best_mae, best_epoch
+def validate(model, epoch, save_path, writer, best_mae, best_epoch, cfg):
     model.eval()
+    mae_sum = 0.0
+
+    test_loader = test_dataset(
+        image_root=os.path.join(cfg.test_path, 'rgb'),
+        polarization_root=os.path.join(cfg.test_path, 'polarization'),
+        gt_root=os.path.join(cfg.test_path, 'gt'),
+        testsize=cfg.trainsize
+    )
+
     with torch.no_grad():
-        mae_sum = 0
-        # test_loader = test_dataset(image_root=opt.test_path + 'Imgs/',
-        #                           gt_root=opt.test_path + 'GT/',
-        #                           testsize=opt.trainsize)
-        test_loader = test_dataset(image_root=opt.test_path + '/rgb/',
-                                   polarization_root=opt.test_path + '/polarization/',
-                            gt_root=opt.test_path + '/gt/',
-                            testsize=opt.trainsize)
-
-        for i in range(test_loader.size):
-            image, polarization_image, gt, name = test_loader.load_data()
+        for _ in range(test_loader.size):
+            img, polar_img, gt, _ = test_loader.load_data()
             gt = np.asarray(gt, np.float32)
-            gt /= (gt.max() + 1e-8)
-            image = image.cuda()
-            polarization_image = polarization_image.cuda()
-            res,res1 = model(image,polarization_image)
-            # res, res1 = model(image)
-            # eval Dice
-            res = F.upsample( res1[-1], size=gt.shape, mode='bilinear', align_corners=False)
-            res = res.sigmoid().data.cpu().numpy().squeeze()
-            res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-            mae_sum += np.sum(np.abs(res - gt)) * 1.0 / (gt.shape[0] * gt.shape[1])
-            
-            # res = F.upsample( res1[-1], size=gt.shape, mode='bilinear', align_corners=False)
-            # res = res.sigmoid().data.cpu().numpy().squeeze()
-            # res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-            # mae_sum += np.sum(np.abs(res - gt)) * 1.0 / (gt.shape[0] * gt.shape[1])
+            gt /= gt.max() + 1e-8
 
-        mae = mae_sum / test_loader.size
-        writer.add_scalar('MAE', torch.tensor(mae), global_step=epoch)
-        print('Epoch: {}, MAE: {}, bestMAE: {}, bestEpoch: {}.'.format(epoch, mae, best_mae, best_epoch))
-        if epoch == 1:
-            best_mae = mae
-        else:
-            if mae < best_mae:
-                best_mae = mae
-                best_epoch = epoch
-                torch.save(model.state_dict(), save_path + 'Net_epoch_best.pth')
-                print('Save state_dict successfully! Best epoch:{}.'.format(epoch))
-        logging.info(
-            '[Val Info]:Epoch:{} MAE:{} bestEpoch:{} bestMAE:{}'.format(epoch, mae, best_epoch, best_mae))
+            img = img.cuda()
+            polar_img = polar_img.cuda()
 
+            _, out = model(img, polar_img)
+            pred = F.interpolate(out[-1], size=gt.shape, mode='bilinear', align_corners=False)
+            pred = pred.sigmoid().cpu().numpy().squeeze()
+            pred = (pred - pred.min()) / (pred.max() - pred.min() + 1e-8)
 
-def train(train_loader, model, optimizer, epoch, test_path):
+            mae_sum += np.sum(np.abs(pred - gt)) / (gt.shape[0] * gt.shape[1])
+
+    mae = mae_sum / test_loader.size
+    writer.add_scalar("Val/MAE", mae, epoch)
+
+    print(f"Epoch {epoch:3d} | MAE {mae:.4f} | Best MAE {best_mae:.4f} | Best Epoch {best_epoch}")
+    logging.info(f"[Val] Epoch {epoch} | MAE {mae} | Best {best_mae}")
+
+    if mae < best_mae:
+        best_mae = mae
+        best_epoch = epoch
+        torch.save(model.state_dict(), os.path.join(save_path, "best_model.pth"))
+        print(f"-> Best model saved at epoch {epoch}")
+
+    return best_mae, best_epoch
+
+def train_epoch(model, loader, optimizer, epoch, total_steps, cfg, loss_recorder):
     model.train()
-    global best
-    size_rates = [1]
-    loss_P2_record = AvgMeter()
-    for i, pack in enumerate(train_loader, start=1):
-        for rate in size_rates:
-            optimizer.zero_grad()
-            # print('this is i',i)
-            # ---- data prepare ----
-            images, polarization_images, gts = pack
-            images = Variable(images).cuda()
-            polarization_images = Variable(polarization_images).cuda()
-            gts = Variable(gts).cuda()
-            # ---- rescale ----
-            trainsize = int(round(opt.trainsize * rate / 32) * 32)
-            if rate != 1:
-                images = F.upsample(images, size=(trainsize, trainsize), mode='bilinear', align_corners=True)
-                polarization_images = F.upsample(polarization_images, size=(trainsize, trainsize), mode='bilinear', align_corners=True)
-                gts = F.upsample(gts, size=(trainsize, trainsize), mode='bilinear', align_corners=True)
-            # ---- forward ----
-            # print('this is trainsize',trainsize)
-            # P1, P2= model(images)
-            P1,P2= model(images,polarization_images)
-            # ---- loss function ----
-            losses = [structure_loss(out, gts) for out in P1]
-            loss_p1=0
-            gamma=0.2
-            # print('iteration num',len(P1))
-            for it in range(len(P1)):
-                loss_p1 += (gamma * it) * losses[it]
-                # loss_p1 = losses[it]
+    loss_recorder.reset()
 
-            losses2 = [structure_loss(out, gts) for out in P2]
-            loss_p2=0
-            gamma2=0.2
-            # print('iteration num',len(P1))
-            for it in range(len(P1)):
-                loss_p2 += (gamma2 * it) * losses2[it]
-                # loss_p2 =  losses2[it]
+    for i, (imgs, polars, gts) in enumerate(loader, 1):
+        optimizer.zero_grad()
 
+        imgs = imgs.cuda()
+        polars = polars.cuda()
+        gts = gts.cuda()
 
-            
+        out1, out2 = model(imgs, polars)
+        loss1 = compute_layered_loss(out1, gts, 0.2)
+        loss2 = compute_layered_loss(out2, gts, 0.2)
+        total_loss = loss1 + loss2
 
-            loss = loss_p1 + loss_p2
-            # ---- backward ----
-            loss.backward()
-            clip_gradient(optimizer, opt.clip)
-            optimizer.step()
-            # ---- recording loss ----
-            if rate == 1:
-                loss_P2_record.update(loss_p1.data, opt.batchsize)
-        # ---- train visualization ----
-        if i % 20 == 0 or i == total_step:
-            print('{} Epoch [{:03d}/{:03d}], Step [{:04d}/{:04d}], '
-                  ' lateral-5: {:0.4f}]'.
-                  format(datetime.now(), epoch, opt.epoch, i, total_step,
-                         loss_P2_record.show()))
-            logging.info('{} Epoch [{:03d}/{:03d}], Step [{:04d}/{:04d}], '
-                  ' lateral-5: {:0.4f}]'.
-                  format(datetime.now(), epoch, opt.epoch, i, total_step,
-                         loss_P2_record.show()))
-    # save model
-    save_path = opt.save_path
-    if epoch % opt.epoch_save == 0:
-        if epoch % 3 == 1:
-            torch.save(model.state_dict(), save_path  + 'PlantCAMO1.pth')
-        elif epoch % 3 ==2 :
-            torch.save(model.state_dict(), save_path  + 'PlantCAMO2.pth')
-        elif epoch % 3 ==0 :
-            torch.save(model.state_dict(), save_path  + 'PlantCAMO3.pth')
+        total_loss.backward()
+        clip_gradient(optimizer, cfg.clip)
+        optimizer.step()
 
-        
-    
-if __name__ == '__main__':
+        loss_recorder.update(loss1.data, cfg.batchsize)
 
-    
-    model_name = 'ablation'
+        if i % 20 == 0 or i == total_steps:
+            msg = f"{datetime.now()} | Epoch {epoch:3d} | Step {i:4d}/{total_steps} | Loss {loss_recorder.show():.4f}"
+            print(msg)
+            logging.info(msg)
 
-    ###############################################
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epoch', type=int,default=150, help='epoch number')
-    parser.add_argument('--lr', type=float,default=1e-4, help='learning rate')
-    parser.add_argument('--optimizer', type=str,default='AdamW', help='choosing optimizer AdamW or SGD')
-    parser.add_argument('--augmentation',default=False, help='choose to do random flip rotation')
-    parser.add_argument('--batchsize', type=int,default=2, help='training batch size')
-    parser.add_argument('--trainsize', type=int,default=704, help='training dataset size,candidate=352,704,1056')
-    parser.add_argument('--clip', type=float,default=0.5, help='gradient clipping margin')
-    parser.add_argument('--load', type=str, default=None, help='train from checkpoints')
-    parser.add_argument('--decay_rate', type=float,default=0.1, help='decay rate of learning rate')
-    parser.add_argument('--decay_epoch', type=int,default=50, help='every n epochs decay learning rate')
-    parser.add_argument('--train_path', type=str,default='./datasets/PlantCAMO1250/train',help='path to train dataset')
-    parser.add_argument('--test_path', type=str,default='./datasets/PlantCAMO1250/test',help='path to testing dataset')
-    parser.add_argument('--save_path', type=str,default='./ckpt/')
-    parser.add_argument('--epoch_save', type=int,default=1, help='every n epochs to save model')
-    opt = parser.parse_args()
+    parser.add_argument('--epoch', type=int, default=150)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--optimizer', type=str, default='AdamW')
+    parser.add_argument('--augmentation', default=False)
+    parser.add_argument('--batchsize', type=int, default=2)
+    parser.add_argument('--trainsize', type=int, default=704)
+    parser.add_argument('--clip', type=float, default=0.5)
+    parser.add_argument('--load', type=str, default=None)
+    parser.add_argument('--decay_rate', type=float, default=0.1)
+    parser.add_argument('--decay_epoch', type=int, default=50)
+    parser.add_argument('--train_path', type=str, default='./datasets/PlantCAMO1250/train')
+    parser.add_argument('--test_path', type=str, default='./datasets/PlantCAMO1250/test')
+    parser.add_argument('--save_path', type=str, default='./ckpt/')
+    parser.add_argument('--epoch_save', type=int, default=1)
+    cfg = parser.parse_args()
 
+    os.makedirs(cfg.save_path, exist_ok=True)
 
-    if not os.path.exists(opt.save_path):
-        os.makedirs(opt.save_path)
+    logging.basicConfig(
+        filename=os.path.join(cfg.save_path, 'train.log'),
+        format='[%(asctime)s] %(levelname)s: %(message)s',
+        level=logging.INFO,
+        filemode='a',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
-    logging.basicConfig(filename=opt.save_path + 'log.log',
-                        format='[%(asctime)s-%(filename)s-%(levelname)s:%(message)s]',
-                        level=logging.INFO, filemode='a', datefmt='%Y-%m-%d %I:%M:%S %p')
-    logging.info("Network-Train")
-
-
-    
     model = MyNet().cuda()
+    if cfg.load:
+        print(f"Loading checkpoint: {cfg.load}")
+        weights = torch.load(cfg.load)
+        load_partial_weights(model, weights)
 
+    print(f"Trainable params: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
 
-    if opt.load is not None:
-        pretrained_dict=torch.load(opt.load)
-        print('!!!!!!Sucefully load model from!!!!!! ', opt.load)
-        load_matched_state_dict(model, pretrained_dict)
-
-    
-
-    print('model paramters',sum(p.numel() for p in model.parameters() if p.requires_grad))
-
-    best = 0
-
-    params = model.parameters()
-
-    if opt.optimizer == 'AdamW':
-        optimizer = torch.optim.AdamW(params, opt.lr, weight_decay=1e-4)
+    if cfg.optimizer == 'AdamW':
+        optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.lr, weight_decay=1e-4)
     else:
-        optimizer = torch.optim.SGD(params, opt.lr, weight_decay=1e-4, momentum=0.9)
+        optimizer = torch.optim.SGD(model.parameters(), lr=cfg.lr, momentum=0.9, weight_decay=1e-4)
 
-    print(optimizer)
-    image_root = '{}/rgb/'.format(opt.train_path)
-    polarization_root = '{}/polarization/'.format(opt.train_path)
-    gt_root = '{}/gt/'.format(opt.train_path)
+    train_loader = get_loader(
+        image_root=os.path.join(cfg.train_path, 'rgb'),
+        polarization_root=os.path.join(cfg.train_path, 'polarization'),
+        gt_root=os.path.join(cfg.train_path, 'gt'),
+        batchsize=cfg.batchsize,
+        trainsize=cfg.trainsize,
+        augmentation=cfg.augmentation
+    )
+    total_steps = len(train_loader)
+    writer = SummaryWriter(os.path.join(cfg.save_path, 'summary'))
+    loss_recorder = AvgMeter()
 
-    train_loader = get_loader(image_root, polarization_root, gt_root, batchsize=opt.batchsize, trainsize=opt.trainsize,
-                              augmentation=opt.augmentation)
-    total_step = len(train_loader)
-
-    writer = SummaryWriter(opt.save_path + 'summary')
-
-    print("#" * 20, "Start Training", "#" * 20)
-    best_mae = 1
+    best_mae = 1.0
     best_epoch = 0
-    for epoch in range(1, opt.epoch + 1):
-        adjust_lr(optimizer, opt.lr, epoch, 0.1, 200)
-        train(train_loader, model, optimizer, epoch, opt.save_path)
-        if epoch % opt.epoch_save==0:
-            val( model, epoch, opt.save_path, writer)
+
+    print("=" * 50)
+    print("Training Started")
+    print("=" * 50)
+
+    for epoch in range(1, cfg.epoch + 1):
+        adjust_lr(optimizer, cfg.lr, epoch, cfg.decay_rate, cfg.decay_epoch)
+        train_epoch(model, train_loader, optimizer, epoch, total_steps, cfg, loss_recorder)
+
+        if epoch % cfg.epoch_save == 0:
+            torch.save(model.state_dict(), os.path.join(cfg.save_path, f"model_epoch_{epoch}.pth"))
+            best_mae, best_epoch = validate(model, epoch, cfg.save_path, writer, best_mae, best_epoch, cfg)
+
+if __name__ == "__main__":
+    main()
